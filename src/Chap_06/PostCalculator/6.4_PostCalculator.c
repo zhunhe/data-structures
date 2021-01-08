facb0e6e5187e0ff3b71c1517b6a60102d182544
zhunhe <xuzhunhe@gmail.com>
2021-01-08 22:12:01 +0900

    Create

---
+++ src/Chap_06/PostCalculator/6.4_PostCalculator.c
@@ -0,0 +1,44 @@
+#include <string.h>
+#include <ctype.h>
+#include "6.4_ListBaseStack.h"
+
+int EvalRPNExp(char exp[])
+{
+	Stack stack;
+	int expLen = strlen(exp);
+	char tok, op1, op2;
+
+	StackInit(&stack);
+
+	for (int i = 0; i < expLen; i++)
+	{
+		tok = exp[i];
+
+		if (isdigit(tok))
+		{
+			SPush(&stack, tok - '0');     // 숫자로 변환하여 PUSH!
+		}
+		else
+		{
+			op2 = SPop(&stack);     // 먼저 꺼낸 값이 두 번째 피연산자!
+			op1 = SPop(&stack);
+
+			switch (tok)
+			{
+				case '+':
+					SPush(&stack, op1+op2);
+					break;
+				case '-':
+					SPush(&stack, op1-op2);
+					break;
+				case '*':
+					SPush(&stack, op1*op2);
+					break;
+				case '/':
+					SPush(&stack, op1/op2);
+					break;
+			}
+		}
+	}
+	return SPop(&stack);
+}
