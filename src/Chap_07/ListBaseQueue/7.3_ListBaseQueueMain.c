5aac2f9c756470f097e7a94c06b084baf480c4a5
zhunhe <xuzhunhe@gmail.com>
2021-01-12 21:11:40 +0900

    Create

---
+++ src/Chap_07/ListBaseQueue/7.3_ListBaseQueueMain.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "7.3_ListBaseQueue.h"
+
+int main(void)
+{
+	// Queue의 생성 및 초기화
+	Queue q;
+	QueueInit(&q);
+
+	// 데이터 넣기
+	for (int i = 1; i <= 5; i++)
+		Enqueue(&q, i);
+
+	// 데이터 꺼내기
+	while(!QIsEmpty(&q))
+		printf("%d ", Dequeue(&q)); 
+
+	return 0;
+}
